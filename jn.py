36efc5ba453b7f222791431db4cabb393ee2da26
lillieharlow <chapman.lillie@gmail.com>
2025-07-15 15:43:31 +1000

    Practicing whilst watching youtube videos on while loops

--- jn.py
+++
@@ -1,6 +0,0 @@
-while True:
-    number = float(input("Enter a number: "))
-    if number < 0:
-        break
-    print ("You entered: ", number)
-    
---
+++ loop.py
@@ -0,0 +1,63 @@
+# While loop excersize:
+
+# Starts with the keyword 'while' followed by the 'test condition' and a :
+# boolean expression - true or false?
+
+'''count = 0
+
+while count < 5:
+    print("I am inside a loop.")
+    print("Looping is interesting.")'''
+    
+# Infitine loop!
+
+# Change this by adding new condition - only run 5 times.
+
+count = 0
+
+while count < 5:
+    print("I am inside a loop.")
+    print("Looping is interesting.")
+    count += 1
+
+print("--------------------------------------------")
+# this will increase count number by 1 each time the code runs through the loop
+
+count = 5
+
+while count <= 10:
+    print(count)
+    count += 1
+
+print("--------------------------------------------")
+# Create code that outputs the multiplication numbers of user input upto 10.
+
+number = int(input("Please enter a number: "))
+             
+count = 1
+while count <= 12:
+    answer = number * count
+    print(f"{number} x {count} = {answer}")
+    count += 1
+
+print("--------------------------------------------")
+# Reverse the output order of the above equation
+
+number = int(input("Please enter a number: "))
+             
+count = 12
+while count >= 1:
+    answer = number * count
+    print(f"{number} x {count} = {answer}")
+    count -= 1
+
+# Breaks in loops
+
+
+
+'''while True:
+    number = float(input("Enter a number: "))
+    if number < 0:
+        break
+    print ("You entered: ", number)'''
+    
