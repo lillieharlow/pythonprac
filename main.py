c0cbbe8ba87a6002d8a414c144fb52330232de67
lillieharlow <chapman.lillie@gmail.com>
2025-07-21 10:15:47 +1000

    "finishing class notes, follow along session with virtual environment."

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+'''
 from packages import square, mathtools
 
 print(mathtools("Hello"))
 
 # main.py needs to be outside the folder that holds the packages !!
 # main.py in the parent folder
+'''
+
+
+import testing
+
+print('add', testing.add(3, 7))
+print('sub', testing.sub(6, 3))
+
---
+++ testing.py
@@ -0,0 +1,7 @@
+# Manual testing
+
+def add(a, b):
+    return a+b
+
+def sub(a, b):
+    return a-b
